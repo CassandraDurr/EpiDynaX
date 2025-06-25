7d76d065e85ec1005e9241f506941a0ee607a69e
CassandraDurr <durrcass@gmail.com>
2025-06-25 15:53:02 +0100

    Experiment increasing beam size and changing temperature

---
+++ epidynax/odeformer_utils.py
@@ -0,0 +1,81 @@
+"""Module for ODEFormer utility functions."""
+
+import numpy as np
+from matplotlib import pyplot as plt
+from odeformer.model import SymbolicTransformerRegressor
+
+
+def get_odeformer_model(
+    beam_size: int = 50, beam_temperature: float = 0.1, rescale: bool = True
+) -> SymbolicTransformerRegressor:
+    """Return a pre-trained ODEFormer model with specified beam parameters.
+
+    Args:
+        beam_size (int, optional): Beam size. Defaults to 50.
+        beam_temperature (float, optional): Beam temperature. Defaults to 0.1.
+        rescale (bool, optional): Whether to rescale. Defaults to True.
+
+    Returns:
+        SymbolicTransformerRegressor: Pre-trained ODEFormer model.
+    """
+    # Initialise the SymbolicTransformerRegressor with pre-trained weights
+    dstr = SymbolicTransformerRegressor(from_pretrained=True, rescale=rescale)
+    dstr.set_model_args({"beam_size": beam_size, "beam_temperature": beam_temperature})
+
+    return dstr
+
+
+def plot_actual_vs_estimated_trajectory(
+    times: np.ndarray,
+    trajectory: np.ndarray,
+    pred_trajectory: np.ndarray,
+    figsize: tuple[int, int] = (12, 8),
+    ylabel: str = "Population",
+) -> plt.Figure:
+    """Plot the actual vs estimated trajectory of an SIR model.
+
+    Args:
+        times (np.ndarray): The time scale/ x axis.
+        trajectory (np.ndarray): The true model trajectory.
+        pred_trajectory (np.ndarray): The estimated trajectory of the SIR model.
+        figsize (tuple[int, int], optional): The plot dimension. Defaults to (12, 8).
+        ylabel (str, optional): The y-axis label. Defaults to "Population".
+
+    Returns:
+        plt.Figure: The actual vs estimated trajectory plot.
+    """
+    fig, ax = plt.subplots(figsize=figsize)
+    # Actuals
+    ax.plot(times, trajectory[:, 0], label="Susceptible (Actual)", color="blue")
+    ax.plot(times, trajectory[:, 1], label="Infected (Actual)", color="red")
+    ax.plot(times, trajectory[:, 2], label="Recovered (Actual)", color="green")
+    # Estimates
+    ax.plot(
+        times,
+        pred_trajectory[:, 0],
+        label="Susceptible (Estimated)",
+        color="blue",
+        linestyle="dashed",
+    )
+    ax.plot(
+        times,
+        pred_trajectory[:, 1],
+        label="Infected (Estimated)",
+        color="red",
+        linestyle="dashed",
+    )
+    ax.plot(
+        times,
+        pred_trajectory[:, 2],
+        label="Recovered (Estimated)",
+        color="green",
+        linestyle="dashed",
+    )
+    # Plot settings
+    ax.set_xlabel("Time (days)")
+    ax.set_ylabel(ylabel)
+    ax.set_title("Actual vs Estimated SIR Model Trajectory")
+    ax.legend()
+    fig.tight_layout()
+
+    return fig
